6b8b7db916b7941d135213cc9ef1f2d5f998b702
Bhumika-Chauhan09 <103961358+Bhumika-Chauhan09@users.noreply.github.com>
2023-03-01 16:15:47 +0530

    Add files via upload

---
+++ comment.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <thread>
+#include <time.h>
+#include <unistd.h>
+#include <string>
+using namespace std;
+
+#define NUM_THREADS 8
+#define MAX 1000000
+pthread_mutex_t mutx;
+
+int data[MAX];
+long global_sum = 0;
+
+void *sum(void *threadid)
+{
+    long tid;
+    long sum = 0;
+    tid = (long)threadid;
+
+    // Specify the data array's processing range for each thread.
+    int range = MAX / NUM_THREADS;
+    int start = tid * range;
+    int end = start + range;
+
+    // Calculate the thread's range's sum of the data elements.
+    for (int i = start; i < end; i++)
+    {
+        sum += data[i];
+    }
+
+    // Use a mutex lock to ensure that global sum is updated sequentially.
+    pthread_mutex_lock(&mutx);
+    global_sum += sum;
+    pthread_mutex_unlock(&mutx);
+    // To enable other threads to access global sum, release the lock.
+
+    //  Exit the thread
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    // Initialize mutex lock
+    pthread_mutex_init(&mutx, NULL);
+
+    // Determine the number of cores on the machine
+    int cores = std::thread::hardware_concurrency();
+    cout << " the number of cores on this machine = " << cores << endl;
+
+    // Initializing data array with random values
+    srand(time(NULL));
+    for (int i = 0; i < MAX; i++)
+    {
+        data[i] = rand() % 20;
+    }
+
+    // Create a pool of threads to process the data
+    pthread_t threads[NUM_THREADS];
+    long tid = 0;
+    for (tid = 0; tid < NUM_THREADS; tid++)
+    {
+        pthread_create(&threads[tid], NULL, sum, (void *)tid);
+    }
+
+    // Wait for all threads to complete before printing the final sum
+    for (tid = 0; tid < NUM_THREADS; tid++)
+    {
+        pthread_join(threads[tid], NULL);
+    }
+
+    cout << "the final sum =" << global_sum << endl;
+}
